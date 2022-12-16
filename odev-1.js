0d8a596f1444dc6182e373efbe0c9cea51c73630
buraktitiz <titizburak10@gmail.com>
2022-12-16 11:22:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ odev-1.js
@@ -0,0 +1,5 @@
+const getName=prompt(`Adınızı yazınız:`)
+document.querySelector("#myName").textContent=getName
+let time=new Date()
+let currentTime=`${time.getFullYear()}-${time.getMonth()}-${time.getDate()}     ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+document.querySelector("#myClock").textContent=currentTime;
